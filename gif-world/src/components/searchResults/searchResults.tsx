51674e843f4bd887ef3ebe387b0a8373c6ff6e62
Olya Balashova <olybalas@amazon.com>
2021-06-08 16:58:30 -0600

    added giphy api

--- gif-world/src/components/searchResults/searchResults.tsx
+++ gif-world/src/components/searchResults/searchResults.tsx
@@ -2,16 +2,51 @@ import React, { Component } from "react";
 import ReactDom from "react-dom";
 import "./searchResults.css";
 
-class SearchResults extends Component<{dataFromParent: string}> {
+class SearchResults extends Component<{dataFromParent: string}, {}> {
     state = {
-        dataFromParent: ""
+        dataFromParent: "",
+        isLoaded: false,
+        items: []
+    }
+
+    componentDidMount() {
+        fetch("http://api.giphy.com/v1/gifs/trending?gif-world&api_key=v39kGDxRW44cU2PinH70vVVl764cftGM&limit=20")
+        .then(res => res.json())
+        .then(result => {
+            this.setState({
+                dataFromParent: "",
+                isLoaded: true,
+                items: result.data
+            });
+            console.log(result)
+        });
     }
     render () {
-        return (
-            <div className="searchResults">
-                Data from parent is: {this.props.dataFromParent}
-            </div>
-        )
+        const { dataFromParent, isLoaded, items } = this.state;
+        if (dataFromParent.length > 0) {
+            return (
+                <div className="searchResults">
+                    Data from parent is: {this.props.dataFromParent}
+                </div>
+            )
+        } else {
+            if (this.state.isLoaded) {
+                return (
+                    <div className="searchResults">
+                        {items.map((item:any) => (
+                            <div key={item.id}>
+                                <img src={item.images.fixed_width.url} className="image"></img>
+                            </div>
+                        ))}
+                    </div>
+                )
+            } else {
+                return (
+                    <div>I am loading</div>
+                )
+            }
+            
+        }
     }
 }
 
