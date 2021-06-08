20624de68c709180c43683106aefbc3c176e83ff
mnemeth66 <mnemeth6@berkeley.edu>
2021-06-08 15:03:53 -0700

    add submit button to search bar

--- gif-world/src/components/searchBar/searchBar.tsx
+++ gif-world/src/components/searchBar/searchBar.tsx
@@ -8,9 +8,18 @@ class SearchBar extends Component<{parentCallback: any},{searchString:string}> {
     state = {
         searchString: ""
     }
+
+    handleSubmit(event:any) {
+        event.preventDefault();
+        if (this.state.searchString.trim()) {
+            this.sendData();
+        } else {
+            alert("Please write item");
+        }
+    }
     onInputChange(event:any) {
         // console.log(event.target.value);
-        this.setState({searchString: event.target.value}, this.sendData);
+        this.setState({searchString: event.target.value});
         // console.log(this.state.searchString);
         // this.sendData(event.target.value);
     }
@@ -22,14 +31,18 @@ class SearchBar extends Component<{parentCallback: any},{searchString:string}> {
     render() {
         return (
             <div className="searchBar">
-                <input
-                    className="input"
-                    name = "searchString"
-                    type = "text"
-                    onChange={this.onInputChange.bind(this)}
-                    placeholder="Input gif to search"
-                    value={this.state.searchString}
-                ></input>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <input
+                        className="input"
+                        name = "searchString"
+                        type = "text"
+                        onChange={this.onInputChange.bind(this)}
+                        placeholder="Input gif to search"
+                        value={this.state.searchString}
+                    ></input>
+                    <input type="submit" value="Search GIF"/>
+                </form>
+                
             </div>
         );
     }
